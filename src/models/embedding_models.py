13a49c874ad9cf613d8a84e40a52e0988a8aeaa5
huynhnhathao <kzdmxq@gmail.com>
2025-07-31 16:38:36 +0700

    update embedding request and response format to be openai compatible

--- src/models/embedding_models.py
+++ src/models/embedding_models.py
@@ -5,13 +5,39 @@ from enum import StrEnum
 
 EmbeddingVector: TypeAlias = list[float]
 
+
 class TextEmbeddingModel(StrEnum):
     QwenEmbedderSmall = "Qwen/Qwen3-Embedding-0.6B"
 
-class TextEmbeddingRequest(BaseModel):
-    model_name: TextEmbeddingModel = Field(default=TextEmbeddingModel.QwenEmbedderSmall, description="huggingface model to run embed")
-    texts: list[str] = Field(description="text to embed")
+
+class OpenAITextEmbeddingRequest(BaseModel):
+    input: list[str] | str = Field(
+        default_factory=list, description="input texts to embed"
+    )
+    model: str = Field(
+        default=TextEmbeddingModel.QwenEmbedderSmall, description="model name"
+    )
+
+    encoding_format: str = Field(default="float")
 
 
-class TextEmbeddingResponse(BaseModel):
-    embeddings: list[EmbeddingVector] = Field(description="list of list of float (2D array) as embedding vectors")
+class OpenAIEmbeddingObject(BaseModel):
+    object: str = Field(default="embedding")
+    embeddings: list[float]
+    index: int = Field(default=0)
+
+
+class EmbeddingUsage(BaseModel):
+    prompt_tokens: int = Field(default=0)
+    total_tokens: int = Field(default=0)
+
+
+class OpenAITextEmbeddingResponse(BaseModel):
+    embeddings: list[EmbeddingVector] = Field(
+        description="list of list of float (2D array) as embedding vectors"
+    )
+
+    object: str = Field(default="list")
+    data: list[OpenAIEmbeddingObject]
+    model: str = Field(description="model name that generated the embeddings")
+    usage: EmbeddingUsage
--- src/services/embedding_service.py
+++ src/services/embedding_service.py
@@ -1,5 +1,6 @@
 from models import embedding_models
 
 
-async def embed_texts(request: embedding_models.TextEmbeddingRequest) -> embedding_models.TextEmbeddingResponse:
-    ...
+async def embed_texts(
+    request: embedding_models.OpenAITextEmbeddingRequest,
+) -> embedding_models.OpenAITextEmbeddingResponse: ...
